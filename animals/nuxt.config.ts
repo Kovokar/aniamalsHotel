52b02ec4b650b742f57c3036fe48bb0771514543
kovokar <pkovokar@gmail.com>
2025-12-01 04:00:38 -0300

    chore: nuxt basics configs

--- animals/nuxt.config.ts
+++ animals/nuxt.config.ts
@@ -1,13 +1,27 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
 import tailwindcss from "@tailwindcss/vite";
 
-// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  // Definindo a porta para o servidor de desenvolvimento
+  devServer: {
+    port: 4000,  // Defina a porta que você deseja
+    host: '0.0.0.0'  // Permite acessar a aplicação de outras máquinas na rede local (opcional)
+  },
+
+  // Data de compatibilidade (quando a compatibilidade foi verificada)
   compatibilityDate: '2025-07-15',
-  devtools: { enabled: true },
-  css: ['./app/main.css'],
+
+  // Desabilitando as ferramentas de desenvolvimento (DevTools)
+  devtools: { enabled: false },
+
+  // Definindo o CSS global (fontawesome no caso)
+  css: ['./app/assets/css/main.css'],
   vite: {
     plugins: [
       tailwindcss(),
     ],
   },
+
+  // Se você estiver utilizando o TailwindCSS (comentado aqui)
+  // modules: ['@nuxtjs/tailwindcss'],
 })
